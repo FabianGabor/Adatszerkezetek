38b741be84b7c3403133265bb44f7323d78ce403
Fábián Gábor <info@fabiangabor.com>
2020-02-23 13:48:56 +0100

    Kiírja az O előtti O mennyiségű karaktereket

--- Adatszerkezetek-02/main.c
+++ Adatszerkezetek-02/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <strings.h>
 
 typedef int tarto[6];
 
 void kiir (tarto t)
 {
     for (int i=1; i<=t[0]; i++)
-        printf("%d ", t[i]);
+        //printf("%d ", t[i]);
+        printf("%c ", t[i] + 'A');
+    printf("\n");
 }
 
 int betesz (tarto t, int x)
@@ -35,15 +38,43 @@ int kivesz (tarto t)
     }
 }
 
+void beker (tarto t)
+{
+    char input[11] = "EABCDAAOOO";
+    //printf("Kerem a jelsorozatot: ");
+    //scanf("%s", input);
+    printf("%d %s \n", (int)strlen(input), input);
+
+    t[0] = 0;
+
+    for (int i=0; i<(int)strlen(input); /*increment inside by 1 or by number of O's */ )
+    {
+        int j=1;
+        for (;input[i+j-1] == 'O' && i-j>=0; j++)
+        {
+            printf("%c", input[i-j]);
+        }
+        i=i+j;
+    }
+    printf("\n");
+}
+
 int main()
 {
-    tarto t = {2,12,25};
+    //tarto t = {2,0,1};
+    tarto t = {0};
 
+    /*
     for (int i=0; i<6; i++)
         printf("%d ", t[i]);
     printf("\n");
+    */
+    //kiir(t);
 
-    kiir(t);
+    //betesz(t, 'A' - 65);
+    //kiir(t);
+
+    beker(t);
 
     return 0;
 }
