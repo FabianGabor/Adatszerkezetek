d2d8e47f01779d4ace7bf6888e292f551d9fc19a
Fábián Gábor <info@fabiangabor.com>
2020-04-03 16:49:05 +0200

    pigeonhole sort

--- Sorting/main.c
+++ Sorting/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <pigeonholeSort.h>
+
+#define MAX_NUM 100
+
+typedef struct data {
+    int arr10   [   10];
+    int arr25   [   25];
+    int arr50   [   50];
+    int arr100  [  100];
+    int arr250  [  250];
+    int arr500  [  500];
+    int arr1000 [ 1000];
+    int arr10000[10000];
+} data;
 
 void printArr (int arr[], int n)
 {
     for (int i=0; i<n; i++)
-        printf("%d ", arr[i]);
+        printf("%2d ", arr[i]);
     printf("\n");
+}
+
+void randomData ( data *data )
+{
+    for (int i=0; i<10; i++)
+        data->arr10[i] = rand() % MAX_NUM;
 }
 
 
 int main()
 {
+    /*
     int arr[] = {8, 3, 2, 7, 4, 6, 8};
     int n = (int) (sizeof(arr) / sizeof(arr[0]));
 
@@ -21,6 +43,27 @@ int main()
 
     printf("Sorted order is: \n");
     printArr(arr, n);
+    */
+
+    data data;
+
+    srand(time(NULL));
+
+    clock_t runtime;
+    runtime = clock();
+
+    for (int i=0; i<5; i++)
+    {
+        randomData( &data );
+        pigeonHoleSort(data.arr10,10, MAX_NUM);
+        printArr(data.arr10, 10);
+    }
+
+
+    runtime = clock() - runtime;
+    double time_taken = ((double)runtime)/CLOCKS_PER_SEC; // in seconds
+    printf("pigeonholeSort took %f seconds to execute \n", time_taken);
+
 
     return 0;
 }
--- Sorting/pigeonholeSort.h
+++ Sorting/pigeonholeSort.h
@@ -1,34 +1,79 @@
 #ifndef PIGEONHOLESORT_H
 #define PIGEONHOLESORT_H
 
-void pigeonholeSort(int arr[], int n)
+void pigeonholeSort(int *array, int n)
 {
+    int *arr;
+    arr = array;
+
     int min = arr[0];
     int max = arr[0];
-    int range, i, j, index;
+    int range, i, j, index = 0;
 
     for (int a=0; a<n; a++)
     {
-        if(arr[a] > max)
+        if (arr[a] > max)
             max = arr[a];
-        if(arr[a] < min)
+        if (arr[a] < min)
             min = arr[a];
     }
 
     range = max - min + 1;
     int phole[range];
 
-    for (i = 0; i<n; i++)
+    for (i = 0; i<range; i++)
         phole[i] = 0;
 
-    for (i = 0; i<n; i++)
+    for (i = 0; i<range; i++)
         phole[arr[i] - min]++;
 
-    index = 0;
-
     for (j=0; j<range; j++)
-        while(phole[j]-->0)
-            arr[index++]=j+min;
+        while(phole[j]-- > 0)
+            arr[index++] = j + min;
 }
+
+void pigeonHoleSort (int arr[], int length, const int max_num)
+{
+    int pigeonHoleArray[ max_num ];
+    int *temp_array = arr;
+
+    int max_element = 0;
+    int min_element = 0;
+
+    int i;
+    int range;
+
+    // Get the min_element and max_element from the given array
+    for (i = 0; i < length; i ++)
+    {
+        if (arr[i] < min_element)
+            min_element = arr[i];
+
+        if (arr[i] > max_element)
+            max_element = arr[i];
+    }
+
+    // Get the range
+    range = ( max_element - min_element ) + 1;
+
+    // Initialize the pigeonHoleArray with zero
+    for ( i = 0; i < range; i++)
+        pigeonHoleArray [i] = 0;
+
+
+    // place the elements in pigeonHoleArray
+    for ( i = 0; i < length; i++, temp_array++)
+        pigeonHoleArray [ *temp_array - min_element ] += 1;
+
+    // copy from pigeonHoleArray to original array
+    for (i = 0, temp_array = &arr[0]; i < range; i++)
+        while (pigeonHoleArray[i] > 0)
+        {
+            pigeonHoleArray[i]--;
+            *temp_array++ = i + min_element;
+        }
+}
+
+
 
 #endif // PIGEONHOLESORT_H
