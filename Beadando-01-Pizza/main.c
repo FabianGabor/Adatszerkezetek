41d737b9e0a19a568f10dcca2628e9ceab1c40ac
Fábián Gábor <info@fabiangabor.com>
2020-04-19 17:07:25 +0200

    free

--- Beadando-01-Pizza/main.c
+++ Beadando-01-Pizza/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+#include <time.h>
+
+#include <setjmp.h>
+
+#define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
+#define CATCH } else {
+#define ETRY } }while(0)
+#define THROW longjmp(ex_buf__, 1)
 
 typedef struct pizza {
     char fajta;
@@ -15,7 +22,7 @@ typedef struct rendeles {
 
 typedef struct havirendeles {
     rendeles *rendeles; // * 1000
-    //int mennyiseg;
+    int mennyiseg;
 } *havirendeles;
 
 int hrMennyiseg = 0;
@@ -37,6 +44,7 @@ char havi_max_pizza(havirendeles h); // 8
 void plusz_rendeles(havirendeles h, rendeles r); // 9
 void plusz_pizza(rendeles r, pizza p); // 10
 
+void randomRendelesek(havirendeles h, rendeles r, pizza p, int n);
 
 int main()
 {
@@ -48,7 +56,7 @@ int main()
     r = uj_rendeles(1, 1, p);
     h = uj_havirendeles(r);
 
-    kiir(h);
+    //kiir(h);
 
     p = uj_pizza(2, 'B');
     r = uj_rendeles(2, 2, p);
@@ -70,9 +78,31 @@ int main()
 
     plusz_rendeles(h, r);
 
-    kiir(h);
+    //kiir(h);
 
-    printf("3. napi max pizza tipus: %c\n", napi_max(h,3));
+    //printf("3. napi max pizza tipus: %c\n", napi_max(h,3));
+
+    //randomRendelesek(h,r,p,10);
+
+
+
+    srand(time(NULL));
+    for (int i=h->mennyiseg; i<10; i++)
+    {
+        p = uj_pizza( rand() % 8 + 1, 'A' + (rand() % 6) );
+        r = uj_rendeles( rand() % 8 + 1, rand() % 29 + 1, p );
+
+        for (int j=0; j< rand() % 6; j++)
+        {
+            p = uj_pizza( rand() % 8 + 1, 'A' + (rand() % 6) );
+            plusz_pizza(r,p);
+        }
+
+        plusz_rendeles(h,r);
+    }
+
+
+    kiir(h);
 
     pizza_torol(p);
     rendeles_torol(r);
@@ -81,12 +111,22 @@ int main()
     return 0;
 }
 
+void randomRendelesek(havirendeles h, rendeles r, pizza p, int n)
+{
+    for (int i=h->mennyiseg; i<n; i++)
+    {
+        p = uj_pizza( rand() % 9, 'A' + (rand() % 6) );
+        r = uj_rendeles( rand() % 9, rand() % 30, p );
+        plusz_rendeles(h,r);
+    }
+}
+
 
 pizza uj_pizza(int db, char fajta)
 {
     if (db > 0 && db <= 9 && fajta >= 'A' && fajta <= 'F')
     {
-        pizza p = malloc(sizeof (pizza));
+        pizza p = malloc(sizeof (p));
         p->db = db;
         p->fajta = fajta;
         return p;
@@ -97,8 +137,11 @@ pizza uj_pizza(int db, char fajta)
 
 void pizza_torol(pizza p)
 {
-    if (p != NULL)
+    if (p->fajta != '\0')
+    {
         free(p);
+        p = NULL;
+    }
 }
 
 
@@ -106,8 +149,8 @@ rendeles uj_rendeles(int futar, int nap, pizza p)
 {
     if (futar >= 1 && futar <= 9 && nap >= 1 && nap <= 30)
     {
-        rendeles r = malloc(sizeof (rendeles));
-        r->pizza = malloc(sizeof (pizza) * 6);
+        rendeles r = malloc(sizeof (r));
+        r->pizza = malloc(sizeof (p) * 6);
 
         r->futar = futar;
         r->nap = nap;
@@ -123,7 +166,12 @@ rendeles uj_rendeles(int futar, int nap, pizza p)
 void rendeles_torol(rendeles r)
 {
     if (r != NULL)
+    {
+        free(r->pizza);
+        r->pizza = NULL;
         free (r);
+        r = NULL;
+    }
 }
 
 
@@ -133,6 +181,7 @@ havirendeles uj_havirendeles(rendeles r)
     h->rendeles = malloc(sizeof(rendeles));
 
     h->rendeles[hrMennyiseg++] = r;
+    h->mennyiseg++;
 
     return h;
 }
@@ -140,13 +189,16 @@ havirendeles uj_havirendeles(rendeles r)
 void havirendeles_torol(havirendeles h)
 {
     if (h != NULL)
+    {
         free(h);
+        h = NULL;
+    }
 }
 
 
 void kiir(havirendeles h)
 {
-    for (int i=0; i<hrMennyiseg; i++) {
+    for (int i=0; i< h->mennyiseg; i++) {
         printf("Rendeles: %d.\n", i+1);
         printf("\tNap: %d \n", h->rendeles[i]->nap);
         printf("\tFutar: %d \n", h->rendeles[i]->futar);
@@ -160,21 +212,34 @@ void kiir(havirendeles h)
 
 void plusz_rendeles(havirendeles h, rendeles r) // 9
 {
-    if (hrMennyiseg < 1000)
+    if (h->mennyiseg < 1000)
     {
-        havirendeles temp = NULL;
-        //h = (havirendeles ) realloc(h, sizeof(havirendeles) * (hrMennyiseg));
-        temp = (havirendeles ) realloc(h, sizeof(havirendeles) * (hrMennyiseg)); // h-t nem irom felul, ha nem sikerul lefoglalni a memoriat
-
-        if (temp == NULL)
+        TRY
         {
-            printf("Hiba a memoria lefoglalasanal a plusz rendelesnek!");
-            exit(1);
-        }
-        else
-            h = temp;
+            havirendeles temp = NULL;
+            //h = (havirendeles ) realloc(h, sizeof(havirendeles) * (hrMennyiseg));
+            //temp = (havirendeles ) realloc(h, sizeof(havirendeles) * (hrMennyiseg)); // h-t nem irom felul, ha nem sikerul lefoglalni a memoriat
+            //temp = (havirendeles ) realloc(h, sizeof(h) + sizeof(h->rendeles[0]) * 2 );
+            temp = (havirendeles ) realloc(h, sizeof(h) + sizeof(r) * 2 );
 
-        h->rendeles[hrMennyiseg++] = r;
+            if (temp == NULL)
+            {
+                printf("Hiba a memoria lefoglalasanal a plusz rendelesnek!");
+                exit(1);
+            }
+            else
+                h = temp;
+
+            h->rendeles[h->mennyiseg++] = r;
+            //THROW;
+        }
+        CATCH
+        {
+            printf("Got Exception!\n");
+        }
+        ETRY;
+
+
     }
 }
 
@@ -190,12 +255,12 @@ void plusz_pizza(rendeles r, pizza p)  // 10
 
 char napi_max(havirendeles h, int nap) // 4
 {
-    if (nap > 0 && nap <= 30 && hrMennyiseg < 1001)
+    if (nap > 0 && nap <= 30 && h->mennyiseg < 1001)
     {
         unsigned int count[6] = {0};
         int max = 0;
 
-        for (int i=0; i<hrMennyiseg; i++)
+        for (int i=0; i< h->mennyiseg; i++)
             if (h->rendeles[i]->nap == nap)
                 for (int j=0; j<6; j++)
                     count[ h->rendeles[i]->pizza[j].fajta - 'A' ] += h->rendeles[i]->pizza[j].db;
@@ -207,12 +272,6 @@ char napi_max(havirendeles h, int nap) // 4
         if (count[max] == 0)
             return 'X';
 
-        /*
-        for (int i=0; i<5; i++)
-            for (int j=i+1; j<6; j++)
-                if (count[i] == count[j])
-                    return 'Y';
-                    */
         for (int i=0; i<6; i++)
             if (count[i] == count[max] && i != max)
                 return 'Y';
@@ -221,4 +280,15 @@ char napi_max(havirendeles h, int nap) // 4
     }
 
     return 'X';
+}
+
+int napi_ossz(havirendeles h, int nap) // 5
+{
+    unsigned int count = 0;
+
+    for (int i=0; i < h->mennyiseg; i++)
+        if (h->rendeles[i]->nap == nap)
+            for (int j=0; j<6; j++)
+                count += h->rendeles[i]->pizza[j].db;
+    return count;
 }
