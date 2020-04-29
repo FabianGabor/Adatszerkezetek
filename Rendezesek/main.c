35b89d549f32f6501356107e3f2488e19c249b2c
Fábián Gábor <info@fabiangabor.com>
2020-04-29 13:32:14 +0200

    Szemelyek beolvasasa

--- Rendezesek/main.c
+++ Rendezesek/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+typedef unsigned int ev;
+typedef unsigned int ho;
+typedef unsigned int nap;
 
 typedef struct szulDatum {
-    unsigned int ev;
-    unsigned int honap;
-    unsigned int nap;
-} szuletesiDatum;
+    ev ev;
+    ho ho;
+    nap nap;
+} szuldatum;
 
 typedef struct szemely {
     char nev[30];
-    szuletesiDatum szulDatum;
+    szuldatum szulDatum;
 } szemely;
 
 typedef szemely *lista;
@@ -28,21 +35,52 @@ void beilleszteses(int *t, int n)
     }
 }
 
+
 lista letrehoz(char * allomanynev)
 {
-    printf("%s", allomanynev);
-
     FILE *file;
     file = fopen(allomanynev, "r");
 
-    char str[9999];
+    char str[30];
+    int i=0;
+    char delim[] = " ";
+    char *ptr;
+
+    lista szemelyek = malloc(sizeof(szemelyek) * 10);
 
     if (file == NULL) perror ("Fajl nyitasi hiba");
     else
     {
         while( fgets(str, 9999, file) != NULL )
         {
-            printf("%s\n", str);
+            //printf("%s\n", str);
+
+            i=0;
+            ptr = strtok(str, delim);
+            while(ptr != NULL)
+            {
+                /*
+                if (atoi(ptr) >0)
+                    data_tmp[i] = atoi(ptr);
+                else
+                    data_tmp[i] = (*ptr - 'A');
+
+                ptr = strtok(NULL, delim);
+                */
+                szemelyek[i].szulDatum.ev = atoi(ptr);
+                ptr = strtok(NULL, delim);
+                szemelyek[i].szulDatum.ho = atoi(ptr);
+                ptr = strtok(NULL, delim);
+                szemelyek[i].szulDatum.nap = atoi(ptr);
+
+                char delim[] = "\0"; // innentol a vegeig, ujradeklaralva, hogy while cikluson kivul alljon vissza " "-re
+                ptr = strtok(NULL, delim);
+                strcpy(szemelyek[i].nev, ptr);
+
+                printf("%s", szemelyek[i].nev);
+                i++;
+                ptr = NULL;
+            }
         }
         fclose (file);
     }
@@ -52,8 +90,8 @@ lista letrehoz(char * allomanynev)
 
 int main()
 {
-    lista lista;
-    lista = letrehoz("in.txt");
+    lista szemelyek;
+    szemelyek = letrehoz("in.txt");
 
     return 0;
 }
