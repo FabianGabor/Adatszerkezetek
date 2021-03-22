d226450d05a386be4eeb9537fadb7cd77f6b6fcb
Fábián Gábor <info@fabiangabor.com>
2021-03-22 14:28:12 +0100

    .

--- ZH3/main.c
+++ ZH3/main.c
@@ -23,36 +23,58 @@ typedef struct fiok {
 typedef struct szekreny {
     fiok fiokSzam[3];
     int db;
-} * szekreny;
+} szekreny;
 
 void kiir(szekreny sz);
+int mennyi_ures_fiok(szekreny sz);
 
 int main() {
-    szekreny szekreny = malloc(sizeof(struct szekreny));
+    szekreny szekreny;
+    szekreny.fiokSzam[0].db = 3; // 0. szekreny fiokok szama
 
-    szekreny->db = 2; // szekrenyek szama
-    szekreny->fiokSzam[0].db = 2; // 0. szekreny fiokok szama
+    for (int i=0; i<3; i++) {
+        szekreny.fiokSzam[i].db = 3;
+        for (int j=0; j<6; j++) {
+            szekreny.fiokSzam[i].fajtaSzam[j].db = 0;
+        }
+    }
 
-    strcpy(szekreny->fiokSzam[0].fajtaSzam[0].fajta, "reszelo");
-    szekreny->fiokSzam[0].fajtaSzam[0].db = 3;
-    strcpy(szekreny->fiokSzam[0].fajtaSzam[1].fajta, "harapofogo");
-    szekreny->fiokSzam[0].fajtaSzam[1].db = 4;
+    strcpy(szekreny.fiokSzam[0].fajtaSzam[0].fajta, "reszelo");
+    szekreny.fiokSzam[0].fajtaSzam[0].db = 3;
+    strcpy(szekreny.fiokSzam[0].fajtaSzam[1].fajta, "harapofogo");
+    szekreny.fiokSzam[0].fajtaSzam[1].db = 4;
 
-    szekreny->fiokSzam[1].db = 1; // 0. szekreny fiokok szama
-    strcpy(szekreny->fiokSzam[1].fajtaSzam[0].fajta, "reszelo");
-    szekreny->fiokSzam[1].fajtaSzam[0].db = 2;
+    szekreny.fiokSzam[1].db = 3; // 1. szekreny fiokok szama
+    strcpy(szekreny.fiokSzam[1].fajtaSzam[0].fajta, "reszelo");
+    szekreny.fiokSzam[1].fajtaSzam[0].db = 2;
 
     kiir(szekreny);
+    printf("Ures fiokok szama: %d \n", mennyi_ures_fiok(szekreny));
 
     return 0;
 }
 
 void kiir(szekreny sz) {
-    for (int szekreny = 0; szekreny < sz->db; szekreny++) {
-        printf("%d fiok: \n", szekreny);
-        for (int fiok = 0; fiok < sz->fiokSzam[szekreny].db; fiok++)
-            printf("%s %d db\n", sz->fiokSzam[szekreny].fajtaSzam[fiok].fajta, sz->fiokSzam[szekreny].fajtaSzam[fiok].db);
+    for (int fiok = 0; fiok < sz.fiokSzam->db; fiok++) {
+        printf("%d fiok: \n", fiok);
+        for (int szerszam = 0; szerszam < sz.fiokSzam[fiok].db; szerszam++)
+            if (sz.fiokSzam[fiok].fajtaSzam[szerszam].db > 0)
+                printf("%s %d db\n", sz.fiokSzam[fiok].fajtaSzam[szerszam].fajta, sz.fiokSzam[fiok].fajtaSzam[szerszam].db);
         printf("\n");
     }
+
     printf("\n");
+}
+
+int mennyi_ures_fiok(szekreny sz) {
+    int ures = 0;
+    int nincsszerszam = 1;
+    for (int fiok = 0; fiok < sz.fiokSzam->db; fiok++) {
+        for (int szerszam = 0; szerszam < sz.fiokSzam[fiok].db; szerszam++)
+            if (sz.fiokSzam[fiok].fajtaSzam[szerszam].db > 0) { // itt kell még debuggolni
+                nincsszerszam = 0;
+            }
+        if (nincsszerszam > 0) ures++;
+    }
+    return ures;
 }
